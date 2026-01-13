2719c3df7822bd14a73e7e51d79339efd3a1e653
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2026-01-13 10:43:20 +0530

    fields changed

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,12 @@ function App() {
           body: JSON.stringify({
             telegram_id: user.id,
             device_id: deviceId,
-            name: user.first_name
+            name: user.first_name,
+            username: user.username,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            photo_url: user.photo_url,
+            auth_date: window.Telegram?.WebApp?.initDataUnsafe?.auth_date
           })
         });
 
